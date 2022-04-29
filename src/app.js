261f895b77716a1907b047d42bfee1bca7efc0e2
Thales Gomes Targino <thalestargino@gmail.com>
2022-04-29 13:59:43 -0300

    fix (get /messages): add from and to messages of each user

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import chalk from 'chalk';
 import dayjs from 'dayjs';
 import { MongoClient } from 'mongodb';
-import { authSchema } from './helpers/schemas/login-name_schema.js';
+import { authSchema, messageBodySchema } from './helpers/schemas/schemas.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -38,7 +38,7 @@ app.get('/messages', async (req, res) => {
         if (limit) {
             const requisicao = await db
                 .collection('messages')
-                .find({ $or: [{ to: 'Todos' }, { to: user }] })
+                .find({ $or: [{ to: 'Todos' }, { to: user }, { from: user }] })
                 .toArray();
             let messages = [...requisicao].reverse().slice(0, limit);
             res.send(messages.reverse());
@@ -87,12 +87,54 @@ app.post('/participants', async (req, res) => {
     }
 });
 
-app.post('/messages', (req, res) => {
-    res.send('OK');
+app.post('/messages', async (req, res) => {
+    const { to, text, type } = req.body;
+    const { user } = req.headers;
+    const bodyValidation = messageBodySchema.validate(req.body);
+    const headerValidation = await db
+        .collection('participants')
+        .findOne({ name: user });
+    if (bodyValidation.hasOwnProperty('error') || !headerValidation) {
+        if (bodyValidation.error) {
+            res.status(422).send(bodyValidation.error.details[0].message);
+        }
+        res.sendStatus(422);
+    } else {
+        try {
+            const message = await db.collection('messages').insertOne({
+                from: user,
+                to,
+                text,
+                type,
+                time: dayjs().format('HH:MM:ss'),
+            });
+            res.sendStatus(201);
+        } catch (err) {
+            console.log(err);
+        }
+    }
 });
 
-app.post('/status', (req, res) => {
-    res.send('OK');
+app.post('/status', async (req, res) => {
+    const { user } = req.headers;
+    try {
+        const userVerification = await db
+            .collection('participants')
+            .findOne({ name: user });
+        if (userVerification) {
+            await db
+                .collection('participants')
+                .updateOne(
+                    { name: user },
+                    { $set: { lastStatus: Date.now() } }
+                );
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 const port = process.env.PORT || 5000;
--- src/helpers/schemas/login-name_schema.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-const authSchema = Joi.object({
-    name: Joi.string().required(),
-});
-
-export { authSchema };
